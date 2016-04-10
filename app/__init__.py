df045cb2e5e53c497aa101719c528b1f17c03a1f
Khanh Ngo <ngokhanhit@gmail.com>
2016-04-10 21:23:27 +0700

    Add ProxyFix() middleware component to fix the HTTPS redirection issue. See !17

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
+from werkzeug.contrib.fixers import ProxyFix
 from flask import Flask
 from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 db = SQLAlchemy(app)
