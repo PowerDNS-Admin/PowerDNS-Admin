ee033f2198969af0a505f19be9d04ce13e929fd6
Khanh Ngo <ngokhanhit@gmail.com>
2016-07-13 21:33:21 +0700

    Add Gravatar to display user's avatar

--- app/lib/utils.py
+++ app/lib/utils.py
@@ -3,6 +3,8 @@ import sys
 import json
 import requests
 import urlparse
+import hashlib
+
 from app import app
 from distutils.version import StrictVersion
 
@@ -145,3 +147,8 @@ def pdns_api_extended_uri(version):
         return "/api/v1"
     else:
         return ""
+
+def email_to_gravatar_url(email, size=100):
+    hash_string = hashlib.md5(email).hexdigest()
+    schema = 'https' if app.config['SSL'] else 'http'
+    return "%s://s.gravatar.com/avatar/%s?s=%s" % (schema, hash_string, size)
