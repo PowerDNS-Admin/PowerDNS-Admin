232d39cca085af6d098f5b22bd54e887a0b111ad
Khanh Ngo <ngokhanhit@gmail.com>
2018-06-11 14:12:04 +0700

    Adjustment in Docker stuff to work with new migration. Add init_data.py script to initialize table data

--- docker/PowerDNS-Admin/entrypoint.sh
+++ docker/PowerDNS-Admin/entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-cd /powerdns-admin && ./create_db.py
+
+if [ ! -d "/powerdns-admin/migrations" ]; then
+    /usr/local/bin/flask db init --directory /powerdns-admin/migrations
+    /usr/local/bin/flask db migrate -m "Init DB" --directory /powerdns-admin/migrations
+    /usr/local/bin/flask db upgrade --directory /powerdns-admin/migrations
+    cd /powerdns-admin && ./init_data.py
+
+else
+    /usr/local/bin/flask db migrate -m "Upgrade BD Schema" --directory /powerdns-admin/migrations
+    /usr/local/bin/flask db upgrade --directory /powerdns-admin/migrations
+fi
+
 /usr/bin/supervisord -c /etc/supervisord.conf
