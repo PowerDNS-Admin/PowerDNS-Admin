b7cd861b38da0aa488e8d74ffbfaf929769a5c9e
Veovis <veovis@kveer.fr>
2019-02-07 18:13:48 +0100

    node_modules does not exists yet?

--- docker/PowerDNS-Admin/entrypoint.sh
+++ docker/PowerDNS-Admin/entrypoint.sh
@@ -76,7 +76,7 @@ mysql -h${PDA_DB_HOST} -u${PDA_DB_USER} -p${PDA_DB_PASSWORD} -P${PDA_DB_PORT} ${
 echo "===> Assets management"
 echo "---> Running Yarn"
 chown -R www-data:www-data /powerdns-admin/app/static
-chown -R www-data:www-data /powerdns-admin/node_modules
+[ -d /powerdns-admin/node_modules ] && chown -R www-data:www-data /powerdns-admin/node_modules
 su-exec www-data yarn install --pure-lockfile
 
 echo "---> Running Flask assets"
