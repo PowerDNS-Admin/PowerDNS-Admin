cf62658e19c291c4a77606737ca131d20c1d263c
Tom Hetmer <TomHetmer@users.noreply.github.com>
2020-09-10 01:18:07 +0700

    do not fail with wildcard PTRs

--- powerdnsadmin/routes/domain.py
+++ powerdnsadmin/routes/domain.py
@@ -91,7 +91,7 @@ def domain(domain_name):
                 # If it is reverse zone and pretty_ipv6_ptr setting
                 # is enabled, we reformat the name for ipv6 records.
                 if Setting().get('pretty_ipv6_ptr') and r[
-                        'type'] == 'PTR' and 'ip6.arpa' in r_name:
+                        'type'] == 'PTR' and 'ip6.arpa' in r_name and '*' not in r_name:
                     r_name = dns.reversename.to_address(
                         dns.name.from_text(r_name))
 
