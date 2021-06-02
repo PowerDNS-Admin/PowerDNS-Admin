fd933f8dbc6ec4ebac0ee6554901d4866096c9ba
Steffen Schwebel <s.schwebel@uvensys.de>
2021-06-02 09:41:08 +0200

    remove unrelated files and changes as best as possible

--- powerdnsadmin/models/setting.py
+++ powerdnsadmin/models/setting.py
@@ -180,7 +180,6 @@ class Setting(db.Model):
             'URI': False
         },
         'ttl_options': '1 minute,5 minutes,30 minutes,60 minutes,24 hours',
-        'otp_field_enabled': True,
         'custom_css': '',
     }
 
