9447e89626543211ee2a34c033fc58f2eb9ccb63
Steffen Schwebel <s.schwebel@uvensys.de>
2021-06-10 14:29:22 +0200

    remove additional settings

--- powerdnsadmin/models/setting.py
+++ powerdnsadmin/models/setting.py
@@ -181,8 +181,6 @@ class Setting(db.Model):
             'URI': False
         },
         'ttl_options': '1 minute,5 minutes,30 minutes,60 minutes,24 hours',
-        'otp_field_enabled': True,
-        'custom_css': '',
     }
 
     def __init__(self, id=None, name=None, value=None):
