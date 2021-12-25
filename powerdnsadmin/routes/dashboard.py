328780e2d41b7120afecf22c83b8d88864eaeaf7
RGanor <44501230+RGanor@users.noreply.github.com>
2021-12-25 16:17:54 +0200

    Revert "Merge branch 'master' into master"
    This reverts commit ca4c145a1809b3fee8510b49fb517a844c3524eb, reversing
    changes made to 7808febad8fb0a56cdecce2d7cdb9a92ec30ff06.

--- powerdnsadmin/routes/dashboard.py
+++ powerdnsadmin/routes/dashboard.py
@@ -208,7 +208,6 @@ def dashboard():
                            history_number=history_number,
                            uptime=uptime,
                            histories=detailedHistories,
-                           histories=history,
                            show_bg_domain_button=show_bg_domain_button,
                            pdns_version=Setting().get('pdns_version'))
 
