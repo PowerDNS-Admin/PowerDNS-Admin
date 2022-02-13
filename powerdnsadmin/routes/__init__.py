990e889ecb059ed40962a06f2de70a66089eb23c
Tim Zhang <ttimasdf@users.noreply.github.com>
2022-02-13 15:17:08 +0800

    Register /api endpoint

--- powerdnsadmin/routes/__init__.py
+++ powerdnsadmin/routes/__init__.py
@@ -6,6 +6,7 @@ from .dashboard import dashboard_bp
 from .domain import domain_bp
 from .admin import admin_bp
 from .api import api_bp
+from .api_list import apilist_bp
 
 
 def init_app(app):
@@ -17,6 +18,7 @@ def init_app(app):
     app.register_blueprint(domain_bp)
     app.register_blueprint(admin_bp)
     app.register_blueprint(api_bp)
+    app.register_blueprint(apilist_bp)
 
     app.register_error_handler(400, handle_bad_request)
     app.register_error_handler(401, handle_unauthorized_access)
