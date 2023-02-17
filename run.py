d12f03c734b47656e34280ff25c49cd10987dfeb
Matt Scott <matt@azorian.solutions>
2023-02-17 18:32:36 -0500

    Corrected dependency conflicts with the recently merged requirements.txt file.
    Updated minor formatting issue with app's run.py file.

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from powerdnsadmin import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug = True, host=app.config.get('BIND_ADDRESS', '127.0.0.1'), port=app.config.get('PORT', '9191'))
+    app.run(debug=True, host=app.config.get('BIND_ADDRESS', '127.0.0.1'), port=app.config.get('PORT', '9191'))
