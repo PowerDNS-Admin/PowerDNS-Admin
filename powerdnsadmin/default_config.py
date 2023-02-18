47b50e5e1e909326b410eacbc9dc97fdfcd8cc83
Matt Scott <matt@azorian.solutions>
2023-02-17 19:35:36 -0500

    Updated default app config to comment out MySQL default settings.

--- powerdnsadmin/default_config.py
+++ powerdnsadmin/default_config.py
@@ -20,12 +20,12 @@ SQLA_DB_NAME = 'pda'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 ### DATABASE - MySQL
-SQLALCHEMY_DATABASE_URI = 'mysql://{}:{}@{}/{}'.format(
-    urllib.parse.quote_plus(SQLA_DB_USER),
-    urllib.parse.quote_plus(SQLA_DB_PASSWORD),
-    SQLA_DB_HOST,
-    SQLA_DB_NAME
-)
+# SQLALCHEMY_DATABASE_URI = 'mysql://{}:{}@{}/{}'.format(
+#     urllib.parse.quote_plus(SQLA_DB_USER),
+#     urllib.parse.quote_plus(SQLA_DB_PASSWORD),
+#     SQLA_DB_HOST,
+#     SQLA_DB_NAME
+# )
 
 ### DATABASE - SQLite
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'pdns.db')
