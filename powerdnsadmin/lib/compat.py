2e68bb53e27b836c10d954685ce448a4770409d9
Mike Holownych <mike.holownych@gmail.com>
2025-07-17 23:57:32 -0400

    Improve Python 3.12 compatibility

--- powerdnsadmin/lib/compat.py
+++ powerdnsadmin/lib/compat.py
@@ -1,11 +1,13 @@
-try:
+"""Compatibility helpers for modules removed in Python 3.12."""
+
+try:  # Python < 3.12
     from distutils.util import strtobool
-except ModuleNotFoundError:  # Python >=3.12
+except ModuleNotFoundError:  # Python >= 3.12
     from setuptools._distutils.util import strtobool
 
-try:
-    from distutils.version import StrictVersion
-except ModuleNotFoundError:
-    from setuptools._distutils.version import StrictVersion
+try:  # Python < 3.12
+    from distutils.version import StrictVersion  # type: ignore
+except ModuleNotFoundError:  # Python >= 3.12
+    from packaging.version import Version as StrictVersion  # noqa: F401
 
 __all__ = ["strtobool", "StrictVersion"]
