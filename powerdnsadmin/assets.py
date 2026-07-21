df302ab97452fc2d5adb61c94540ee8be72dc189
AgentTNT <ttodd@ttodd.com>
2026-07-21 00:46:21 -0400

    feat(ui): migrate frontend to AdminLTE 4 and Bootstrap 5
    - upgrade AdminLTE to 4.1.0 and Bootstrap to 5.3.8
    - switch DataTables integration from Bootstrap 4 to Bootstrap 5
    - modernize the base layout, sidebar, breadcrumbs, tabs, modals, and authentication pages
    - restore dynamic dashboard tabs, navigation, settings access, and modal behavior
    - replace bootstrap-datepicker with native date inputs
    - remove obsolete Mozilla CSS properties during asset compilation
    - enforce immutable Yarn 4 Docker-only dependency installation
    - prevent repository node_modules generation
    - fix case-insensitive duplicate-user detection and rollback failed database transactions
    - add frontend migration and duplicate-user regression tests

--- powerdnsadmin/assets.py
+++ powerdnsadmin/assets.py
@@ -1,3 +1,5 @@
+import re
+
 from flask_assets import Bundle, Environment, Filter
 
 
@@ -9,18 +11,30 @@ class ConcatFilter(Filter):
         out.write(';'.join([h.data() for h, info in hunks]))
 
 
+class RemoveLegacyCssPropertiesFilter(Filter):
+    """Remove obsolete vendor declarations rejected by current browsers."""
+
+    def input(self, _in, out, **kwargs):
+        css = _in.read()
+        css = re.sub(
+            r'(^|[;{])\s*-moz-border-radius\s*:[^;{}]+;',
+            r'\1',
+            css,
+            flags=re.MULTILINE,
+        )
+        out.write(css)
+
+
 css_login = Bundle(
     'node_modules/@fortawesome/fontawesome-free/css/all.css',
-    'node_modules/icheck/skins/square/blue.css',
     'node_modules/admin-lte/dist/css/adminlte.css',
-    filters=('rcssmin', 'cssrewrite'),
+    filters=(RemoveLegacyCssPropertiesFilter, 'rcssmin', 'cssrewrite'),
     output='generated/login.css')
 
 js_login = Bundle(
     'node_modules/jquery/dist/jquery.js',
-    'node_modules/bootstrap/dist/js/bootstrap.js',
-    'node_modules/icheck/icheck.js',
     'node_modules/knockout/build/output/knockout-latest.js',
+    'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
     'custom/js/custom.js',
     filters=(ConcatFilter, 'rjsmin'),
     output='generated/login.js')
@@ -31,36 +45,34 @@ js_validation = Bundle(
 
 css_main = Bundle(
     'node_modules/@fortawesome/fontawesome-free/css/all.css',
-    'node_modules/datatables.net-bs4/css/dataTables.bootstrap4.css',
     'node_modules/icheck/skins/square/blue.css',
     'node_modules/multiselect/css/multi-select.css',
     'node_modules/admin-lte/dist/css/adminlte.css',
+    'node_modules/datatables.net-bs5/css/dataTables.bootstrap5.css',
     'custom/css/custom.css',
-    'node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.css',
-    filters=('rcssmin', 'cssrewrite'),
+    filters=(RemoveLegacyCssPropertiesFilter, 'rcssmin', 'cssrewrite'),
     output='generated/main.css')
 
 js_main = Bundle(
     'node_modules/jquery/dist/jquery.js',
     'node_modules/jquery-ui-dist/jquery-ui.js',
-    'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
-    'node_modules/datatables.net/js/jquery.dataTables.js',
-    'node_modules/datatables.net-bs4/js/dataTables.bootstrap4.js',
     'node_modules/jquery-sparkline/jquery.sparkline.js',
     'node_modules/jquery-slimscroll/jquery.slimscroll.js',
     'node_modules/jquery-validation/dist/jquery.validate.js',
     'node_modules/icheck/icheck.js',
     'node_modules/fastclick/lib/fastclick.js',
     'node_modules/moment/moment.js',
-    'node_modules/admin-lte/dist/js/adminlte.js',
     'node_modules/multiselect/js/jquery.multi-select.js',
-    'node_modules/datatables.net-plugins/sorting/natural.js',
     'node_modules/jtimeout/src/jTimeout.js',
     'node_modules/jquery.quicksearch/src/jquery.quicksearch.js',
+    'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+    'node_modules/datatables.net/js/jquery.dataTables.js',
+    'node_modules/datatables.net-bs5/js/dataTables.bootstrap5.js',
+    'node_modules/datatables.net-plugins/sorting/natural.js',
+    'node_modules/admin-lte/dist/js/adminlte.js',
     'node_modules/knockout/build/output/knockout-latest.js',
     'custom/js/app-authentication-settings-editor.js',
     'custom/js/custom.js',
-    'node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.js',
     filters=(ConcatFilter, 'rjsmin'),
     output='generated/main.js')
 
